adccede15af25c623df0f600f7fd367e6ced2d4c
Puria Golgoonnejad <puriagit@gmail.com>
2020-02-08 19:51:37 +1100

    home route has been changed to dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
