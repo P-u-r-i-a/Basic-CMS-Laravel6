0347a20e331e1556cd9f428b1facda16373a4b03
Puria Golgoonnejad <puriagit@gmail.com>
2020-02-29 23:02:00 +1100

    showing current active link

--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -2,19 +2,19 @@
     <div class="sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link active" href="{{ route('dashboard') }}">
+                <a class="nav-link {{url()->current() == route('dashboard')?'active':''}}" href="{{ route('dashboard') }}">
                     <i data-feather="home"></i>   
                     Dashboard <span class="sr-only">(current)</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('posts.index') }}">
+                <a class="nav-link {{url()->current() == route('posts.index')?'active':''}}" href="{{ route('posts.index') }}">
                     <i data-feather="list"></i>
                     Posts
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('categories.index') }}">
+                <a class="nav-link {{url()->current() == route('categories.index')?'active':''}} " href="{{ route('categories.index') }}">
                     <i data-feather="list"></i>
                     Categories
                 </a>
@@ -25,13 +25,13 @@
         </h6>
         <ul class="nav flex-column mb-2">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('posts.create') }}">
+                <a class="nav-link {{url()->current() == route('posts.create')?'active':''}}" href="{{ route('posts.create') }}">
                     <i data-feather="plus-circle"></i>
                     New Post
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('categories.create') }}">
+                <a class="nav-link {{url()->current() == route('categories.create')?'active':''}}" href="{{ route('categories.create') }}">
                     <i data-feather="plus-circle"></i>
                     New Category
                 </a>
