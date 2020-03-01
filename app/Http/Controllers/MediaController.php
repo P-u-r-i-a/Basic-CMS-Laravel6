d95487744ed1b4af847080a37558d8ca8b40b323
Puria Golgoonnejad <puriagit@gmail.com>
2020-03-01 21:44:48 +1100

    add Media model and controller

---
+++ app/Http/Controllers/MediaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Media;
+use Illuminate\Http\Request;
+
+class MediaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // TODO
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // TODO
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // TODO
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Media  $media
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Media $media)
+    {
+        // TODO
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Media  $media
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Media $media)
+    {
+        // TODO
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Media  $media
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Media $media)
+    {
+        // TODO
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Media  $media
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Media $media)
+    {
+        // TODO
+    }
+}
---
+++ database/migrations/2020_02_29_120740_create_media_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->text('path');
+            $table->string('mime');
+            $table->mediumText('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
